125dacd898435566d6cc59cd254dcf10ae791a2a
Nicolas Unger <nicounger94@gmail.com>
2017-05-12 18:38:43 -0300

    Correccion de bugs

--- src/ising.c
+++ src/ising.c
@@ -10,8 +10,8 @@ int main(int argc, char **argv)
     int n = 32; //Tamano de red
     int *lattice = malloc(n * n * sizeof(int));
     float prob = 0.5;
-    float T = 0.3;
-    int niter = 2000;
+    float T = 2;
+    int niter = 20000;
     srand(time(NULL));
     fill_lattice(lattice, n, prob);
     printf("%d\n", energia(lattice,n));
@@ -21,6 +21,7 @@ int main(int argc, char **argv)
     {
         metropolis(lattice, n, T);
     }
+
     printf("%d\n", energia(lattice,n));
     print_lattice(lattice, n);
 
