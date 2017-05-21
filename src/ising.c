bfbcc6e7bedaa785476787d9985d6e961678fbc3
Nicolas Unger <nicounger94@gmail.com>
2017-05-21 18:24:33 -0300

    Corregi la variacion que la estaba calculando mal. Tambien agregue mas puntos para la temperatura cerca de la temperatura critica.

--- src/ising.c
+++ src/ising.c
@@ -12,19 +12,23 @@ int main(int argc, char **argv)
     int *lattice = malloc(n * n * sizeof(int));
     float prob = 0.5; // Probabilidad inicial de llenado
     int i,j;
-    int nT = 200;
+    int nT = 260;
     int start = 5;
+    int fino1 = 3;
+    int fino2 = 2;
     int stop = 0.1;
     float *T = malloc(nT*sizeof(float)); // Temperatura
-    for(j=0;j<nT;j++) T[j] = start + (stop-start)*((float)j/nT);
+    for(j=0;j<80;j++) T[j] = start + (fino1-start)*((float)j/80);
+    for(j=80;j<180;j++) T[j] = fino1 + (fino2-fino1)*(((float)j-80)/100);
+    for(j=180;j<nT;j++) T[j] = fino2 + (stop-fino2)*(((float)j-180)/80);
 
     float J = 1; // J de la energia
     float B = 0.0; // Campo magnetico
     int nterm = 5000; // Nr de pasos para la pre-termalizacion
-    int niter = 500000; // Nr de iteraciones
+    int niter = 1000000; // Nr de iteraciones
     int delta_mag; //Cambio de magnetizacion en cada iteracion
-    float *ene = malloc(nT*sizeof(float)); //Va guardando energia para T
-    float *mag = malloc(nT*sizeof(float)); //Va guardando magnetizacion para T
+    float *ene = malloc(nT*sizeof(float)); //Energia media vs T
+    float *mag = malloc(nT*sizeof(float)); //Magnetizacion media vs T
     float *e_iter = malloc(niter*sizeof(float)); //Energia en todos los niter
     float *m_iter = malloc(niter*sizeof(float)); //Magnetizacion en todos los niter
     float *var_e = malloc(nT*sizeof(float)); //Variacion de la energia
@@ -68,8 +72,8 @@ int main(int argc, char **argv)
         var_e[j]=0;
         for(i=0;i<niter;i++)
         {
-            var_e[j] = (e_iter[i] - e_avg)*(e_iter[i] - e_avg);
-            var_m[j] = (m_iter[i] - m_avg)*(m_iter[i] - m_avg);
+            var_e[j] += (e_iter[i] - e_avg)*(e_iter[i] - e_avg);
+            var_m[j] += (m_iter[i] - m_avg)*(m_iter[i] - m_avg);
         }
         var_e[j] = sqrt(var_e[j]/niter);
         var_m[j] = sqrt(var_m[j]/niter);
