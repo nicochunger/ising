5b1bc7c025fcc8da52782968a933eba25fb04684
Nicolas Unger <nicounger94@gmail.com>
2017-05-30 19:19:11 -0300

    Corri simulacion para disintos tamanos de red.

---
+++ src/python/tp2_tam_finito.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+path = "/home/nunger/Documents/ising/res/"
+
+energia = np.loadtxt(path + "tp2_energia_n512.txt")
+mag = np.loadtxt(path + "tp2_mag_n256.txt")
+var_e = np.loadtxt(path + "tp2_varene_n64.txt")
+var_m = np.loadtxt(path + "tp2_varmag_n64.txt")
+temp = np.loadtxt(path + "tp2_temp_260.txt")
+
+plt.plot(temp,var_m,"b.")
+plt.title("Magnetizacion")
